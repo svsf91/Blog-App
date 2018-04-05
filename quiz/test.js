a1e00cb80f0d16bcbda5d8af45b2fb55bd79cf6b
Hugh <deng.ch@husky.neu.edu>
2018-04-04 18:23:16 -0700

    new

--- quiz/test.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(process.cwd());
-    res.end();
-}).listen(4000);
--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.use(function(req, res) {
   // Use res.sendfile, as it streams instead of reading the file into memory.
   res.sendFile(__dirname + '/dist/index.html');
 });
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
